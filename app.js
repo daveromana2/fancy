869b3ccbc9d2bda5c6460cdae7af8e13fb8f6cfc
heicx_sudo <heicx_sudo@163.com>
2015-04-10 19:54:54 +0800

    admin post router

--- app.js
+++ app.js
@@ -67,7 +67,33 @@ app.post("/admin/movie/new", function(res, req) {
 			if(err) {
 				conole.log(err);
 			}else {
-				
+				_movie = _.extend(movie, movieObj);
+				_movie.save(function(err, movie) {
+					if(err) {
+						console.log(err);
+					}else {
+						res.redirect("/movie/" + movie.id)
+					}
+				})
+			}
+		})
+	}else {
+		_movie = new Movie({
+			doctor: movieObj.doctor,
+			title: movieObj.title,
+			country: movieObj.country,
+			language: movieObj.language,
+			year: movieObj.year,
+			poster: movieObj.poster,
+			summary: movieObj.summary,
+			flash: movieObj.flash,
+		})
+
+		_movie.save(function(err, movie) {
+			if(err) {
+				console.log(err);
+			}else {
+				res.redirect("/movie/" + movie.id)
 			}
 		})
 	}
@@ -78,15 +104,7 @@ app.post("/admin/movie/new", function(res, req) {
 app.get("/admin/list",function(req, res) {
 	res.render("list", {
 		title : "列表页",
-		movies: [{
-			title: "王牌特工",
-			_id: 1,
-			doctor: "马修·沃恩",
-			country: "美国",
-			year: 2014,
-			language: "英文",
-			flash: "http://static1.mtime.cn/20150129134726/flash/newvideoplayer.swf?vid=53273&mid=204906&title=%E7%8E%8B%E7%89%8C%E7%89%B9%E5%B7%A5%EF%BC%9A%E7%89%B9%E5%B7%A5%E5%AD%A6%E9%99%A2%20%E4%B8%AD%E6%96%87%E7%89%88%E7%BB%88%E6%9E%81%E9%A2%84%E5%91%8A%E7%89%87&refurl=http%3A%2F%2Fvideo.mtime.com%2F53273%2F%3Fmid%3D204906&autoplay=0"
-		}]
+		movies: movie
 	});
 })
 
